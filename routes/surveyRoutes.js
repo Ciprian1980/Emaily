95b9b1f80c744e612402aee0899f4b75be14a25f
Ciprian Popa <ciprian_popa2002@yahoo.com>
2021-09-09 17:56:46 -0400

    Error 500 on ngrok

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -12,8 +15,30 @@ module.exports = app => {
     res.send('Thanks for voting!');
   });
 
+  //extracting just the url and surveyId and choice
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
+    console.log(req.body)
+    const events = _.map(req.body, (event) => {
+      //out of entire url, we take only '/api/surveys/5971/yes'.
+      const pathname = new URL(event.url).pathname; 
+      //extracting the surveyId and choice out of the path.
+      const p = new Path('/api/surveys/:surveyId/:choice');
+      const match = p.test(pathname);
+      console.log(match)
+      //if there is a match, return just the email, surveyId and the choice.
+      if (match) {
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    })
+    //we iterate over the events with lodash function - compact,
+    //which iterates over the array and removes all elements that are undefined.
+    const compactEvents = _.compact(events);
+    
+    //_.uniqBy is another lodash function that iterates over the array 
+    //compactEvents and if they find email and surveyId duplicates, it removes them.
+    //the user can't vote twice - with same email on the same survey.
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    //console.log(uniqueEvents);
     res.send({});
   })
 
@@ -32,7 +57,7 @@ module.exports = app => {
         .split(',')
         .map(email => ({ email })),
       _user: req.user.id,
-      dateSent: Date.now()
+      dateSent: Date.now(),
     });
     //First argument - survey
     //Everytime we want to send out an email it will have
