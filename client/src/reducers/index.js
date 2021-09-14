808f6cfe604512323130442b747e45f4516e0c67
Ciprian Popa <ciprian_popa2002@yahoo.com>
2021-09-14 17:26:51 -0400

    User can see survey results

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer  from './authReducer';
+import surveysReducer from './surveysReducers';
 
 
 //we name auth and form a descriptive name aso we can access later the value 
 export default combineReducers({
     auth: authReducer,
-    form: reduxForm
+    form: reduxForm,
+    surveys: surveysReducer
 })
 
